58a2b1bcaf877a9c32ad4bb12ee751366650c114
Amit Dubey <amitdubey1826@gmail.com>
2020-10-04 23:33:23 +0530

    Added async validators, set and patch value and reset form

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   forbiddenUsernames: Array<string> = ['Admin', 'Amit'];
+  forbiddenEmailDomains: Array<string> = ['yopmail.com', 'test.com'];
 
   signUpForm: FormGroup;
 
@@ -16,7 +18,7 @@ export class AppComponent implements OnInit {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.handleForbiddenUsernames.bind(this)]),
-          email: new FormControl(null, [Validators.required, Validators.email])
+          email: new FormControl(null, [Validators.required, Validators.email], this.handleForbiddenEmails.bind(this))
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -38,5 +40,34 @@ export class AppComponent implements OnInit {
     }
     // custom validator must always return null if its valid
     return null;
+  }
+
+  handleForbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const email = control.value.toString();
+        const emailDomain = email.substring(email.indexOf('@') + 1);
+        if (this.forbiddenEmailDomains.findIndex((domain) => domain.toLowerCase() == emailDomain.toLowerCase()) !== -1) {
+          resolve({'isEmailForbidden': true});
+        } else {
+          resolve(null);
+        }
+      },1500)
+    })
+  }
+
+  // used to set and patch values into the form
+  onLoadDefault() {
+    this.signUpForm.patchValue({
+      userData: {
+        username: 'Amit',
+        email: 'amit@test.com'
+      },
+      gender: 'male'
+    })
+  }
+
+  onReset() {
+    this.signUpForm.reset();
   }
 }
